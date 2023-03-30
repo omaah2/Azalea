ec4d9570218cfa458d8a65c25c4320df93df5bd1
omaah <ummkhaladoluwatosin@gmail.com>
2023-03-30 06:09:36 -0700

    login-page

---
+++ src/components/welcome.jsx
@@ -0,0 +1,20 @@
+import "../css/main.css"
+import styled from "styled-components";
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
+
+export default function Welcome() {
+return (
+<>
+<div className="maincontainer">
+     <h2>Welcome</h2>
+            <div className="inputcontainer">
+              <input type="email" id="email" pattern=".+@name\.com" size="30" required />
+              <input type="password" id="pass" name="password" minlength="8" required></input>
+            </div>
+</div>
+</>
+);
+}
+
+
+
