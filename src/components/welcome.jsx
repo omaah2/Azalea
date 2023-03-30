97cd0dcd5d0a0858837819e0e6799ed487f5d314
omaah <ummkhaladoluwatosin@gmail.com>
2023-03-30 06:26:57 -0700

    name

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -8,7 +8,7 @@ return (
 <div className="maincontainer">
      <h2>Welcome</h2>
             <div className="inputcontainer">
-              <input type="email" id="email" pattern=".+@name\.com" size="30" required />
+              <input type="email" id="email" name="email" pattern=".+@name\.com" size="30" required />
               <input type="password" id="pass" name="password" minlength="8" required></input>
             </div>
 </div>
